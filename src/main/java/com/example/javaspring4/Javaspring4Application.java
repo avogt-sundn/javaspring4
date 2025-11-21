22c5cbf38fe9a5574bb5f4b22ef3a7661616694b
avogt-sundn <avogt@s-und-n.de>
2025-11-21 12:52:20 +0000

    initial java project with devcontainer

---
+++ src/main/java/com/example/javaspring4/Javaspring4Application.java
@@ -0,0 +1,13 @@
+package com.example.javaspring4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javaspring4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javaspring4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaspring4/Javaspring4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspring4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javaspring4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
